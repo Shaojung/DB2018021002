245b2e3cd38cbcd1eed65c26717a14f7f3957281
Shaojung <shaojung@gmail.com>
2018-02-10 10:36:22 +0800

    on item click listener

--- app/src/main/java/com/example/student/db2018021002/MainActivity.java
+++ app/src/main/java/com/example/student/db2018021002/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.student.db2018021002;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.mymenu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.menu1:
+                Toast.makeText(MainActivity.this, "Menu 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu2:
+                Toast.makeText(MainActivity.this, "Menu 2", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
